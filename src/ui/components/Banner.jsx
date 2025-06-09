508c1efe79562ef2014e9660f17da8207c88b202
MarkoMinovski <minovskimarco@gmail.com>
2025-06-09 21:32:48 +0200

    Added some basic animations (Unfinished)

--- src/ui/components/Banner.jsx
+++ src/ui/components/Banner.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import '../../index.css'
-import {Box, Container, Typography} from "@mui/material";
+import { motion } from "framer-motion";
 
 
 const Banner = () => {
     return (
-        <div className={"w-screen h-96 flex flex-col items-center justify-center italic"}>
-            <h1 className={"font-light text-5xl mb-10"}>Marko Minovski</h1>
-            <h5 className={"text-xl"}>Software Engineering Student @ FINKI</h5>
-        </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+        >
+            <div className={"w-screen h-96 flex flex-col items-center justify-center italic"}>
+                <h1 className={"font-light text-5xl mb-10"}>Marko Minovski</h1>
+                <h5 className={"text-xl"}>Software Engineering Student @ FINKI</h5>
+            </div>
+        </motion.div>
+
     )
 }
 
